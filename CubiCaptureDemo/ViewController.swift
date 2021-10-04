03a97212d4a4540a0741fee94c5306477f438bb0
Marnix Arnold <marnix.arnold@cubicasa.com>
2021-10-04 11:30:14 +0200

    Release/2.6.2 (#14)
    * Updated release notes.
    
    * Updated localizable.strings
    
    * Updated SDK pod, version 2.6.2
    
    * Updated README, added to project
    
    * Updated example to use the playback view, portrait/landscape orientation

--- CubiCaptureDemo/ViewController.swift
+++ CubiCaptureDemo/ViewController.swift
@@ -11,12 +11,21 @@ import UIKit
 import CubiCapture
 
 class ViewController: UIViewController {
-    
+
+    private var projectLocation: URL?
+
+    @IBOutlet weak var playbackButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        playbackButton.isEnabled = false
     }
-    
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+
     @IBAction func startPressed(_ sender: UIButton) {
         // Initiate CCCapture
         let ccCapture = CCCapture()
@@ -26,10 +35,28 @@ class ViewController: UIViewController {
         present(ccCapture, animated: true, completion: nil)
     }
 
-    private func endScan(_ controller: CCCapture) {
-        // End the session
-        controller.endCaptureSession()
+    @IBAction func playbackPressed(_ sender: Any) {
+        guard let projectLocation = projectLocation else {
+            return
+        }
+        do {
+            let playbackVC = try CCScanPlaybackViewController(projectLocation: projectLocation)
+            let navController = UINavigationController(rootViewController: playbackVC)
+            playbackVC.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close,
+                                                                          target: self,
+                                                                          action: #selector(dismissPlayback))
+            navController.modalPresentationStyle = .overFullScreen
+            self.present(navController, animated: true, completion: nil)
+        } catch {
+            print("Error instantiating playback controller: \(error.localizedDescription)")
+        }
+    }
 
+    @objc func dismissPlayback() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    private func endScan(_ controller: CCCapture) {
         DispatchQueue.main.async {
             self.dismiss(animated: true, completion: nil)
         }
@@ -45,98 +72,26 @@ extension ViewController: CCCaptureDelegate {
     func zippedDataLocation(_ controller: CCCapture, location: URL) {
         // You can now upload the file in location to your server and use our server-to-server API to process the capture
         print("Location received \(location)")
+        projectLocation = location
+        playbackButton.isEnabled = true
     }
     
     func messenger(_ controller: CCCapture, errorCode: Int, message: String) {
         // This method is called whenever there is a change in the capture process
         print("Error code and message received \(errorCode) and \(message)")
         switch errorCode {
-        case 0:
-            // Device turned to landscape orientation
+        case 0, 1, 2, 5, 7, 23, 24, 26, 27, 28, 32:
+            // Positive
             break
-        case 1:
-            // Started recording
+        case 8, 9, 17, 21, 22, 29, 30, 31, 53, 72, 73, 74, 77, 78, 85, 86:
+            // Worrysome
             break
-        case 2:
-            // Finished recording
+        case 3, 13, 51, 54, 56, 57, 58, 59, 82, 83, 90:
+            // Terminal, the SDK kill the scan with scanTrackingFailed
             break
-        case 3:
-            // Recording was successfull but too short
+        case 15, 18, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 70, 71, 75, 76, 80, 81:
+            // Information
             break
-        case 5:
-            endScan(controller)
-        case 7:
-            // Zipping is done.
-            break
-        case 23:
-            // Not scanning ceiling or floor anymore
-            break
-        case 8:
-            // ARKit TrackingFailureReason: Insuffient light
-            break
-        case 9:
-            break
-        // ARKit TrackingFailureReason: excessive motion
-        case 17:
-            // Device is in reverse-landscape orientation
-            break
-        case 21:
-            // Scanning floor
-            break
-        case 22:
-            // Scanning ceiling
-            break
-        case 13:
-            // Scan drifted! Position changed by over 10 meters during 2 second interval
-            endScan(controller)
-        case 40 :
-            // Started listening for speech.
-            break
-        case 41:
-            // Listening finished, displaying recognition results.
-            break
-        case 42:
-            // Recognition result '' was chosen.
-            break
-        case 43:
-            // Recognition results canceled.
-            break
-        case 44:
-            // Speech recognition aborted.
-            break
-        case 45:
-            // No speech recognition results.
-            break
-        case 46:
-            // All recognition results over the max length of 40 characters.
-            break
-        case 47:
-            // Requesting RECORD_AUDIO permission.
-            break
-        case 48:
-            // User granted RECORD_AUDIO permission.
-            break
-        case 49:
-            // User denied RECORD_AUDIO permission.
-            break
-        case 51:
-            // Zipping the scan failed
-            fallthrough
-        case 54:
-            // Failed to write arkitdata to file
-            fallthrough
-        case 56:
-            // Failed to write config to file
-            fallthrough
-        case 58:
-            // Could not find filepath
-            fallthrough
-        case 59:
-            // Depth map or AVasset writer could not take frames in
-            fallthrough
-        case 15:
-            // Error, removing scan
-            endScan(controller)
         default:
             break
         }
@@ -144,6 +99,7 @@ extension ViewController: CCCaptureDelegate {
     
     func processReadyDelegationFunc(_ controller: CCCapture) {
         print("processReady")
+        endScan(controller)
     }
 
     func scanTrackingFailed(_ controller: CCCapture) {
